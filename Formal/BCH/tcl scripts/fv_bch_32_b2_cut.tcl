b3630aa5e86f930570f41a465bdd8a408adf5c5f
JuanOspina <juancospina98@gmail.com>
2026-05-26 11:24:47 +0200

    clean tcl scripts

--- Formal/BCH/tcl scripts/fv_bch_32_b2_cut.tcl
+++ Formal/BCH/tcl scripts/fv_bch_32_b2_cut.tcl
@@ -2,29 +2,6 @@
 
 # TCL-script for OneSpin (Siemens EDA)
 
-################################################################################
-# Configuration
-
-# Tell the tool to use database (and create one unless it exists)
-# When 1: Tool will     create &     load the database
-# When 0: Tool will not create & not load the database
-set use_setup_database 0
-
-# Select what actions the tool should automatically perform
-# Check:   When true  it          performs proofs for all properties of the default task
-#          When false it does not perform  proofs for all properties of the default task
-set auto_check 0
-# Witness: When true  it          computes witnesses for all properties of the default task
-#          When false it does not compute  witnesses for all properties of the default task
-set auto_witness 0
-
-# Tell the tool to exit after the execution of the tcl-script finishes
-# When 1: Tool will exit
-# When 0: Tool will remain in interactive mode
-set exit_after_execution 0
-
-################################################################################
-# Script - No change required below this line
 
 # Change working directory to the directory of the script
 # Eliminate every symbolic link
@@ -49,9 +26,7 @@ set_session_option -naming_style sv
 
 # Load Setup Database
 set setup_database_name setup.onespin
-if {$use_setup_database && [file isdirectory $setup_database_name]} {
-    load_database -force $setup_database_name
-} else {
+
 
 ###############
 # Load Design #
@@ -97,49 +72,13 @@ set_cut_signals {memory_wrapper.u_sram.din0 memory_wrapper.encoder_inst.data_i}
 
 cut_signals {memory_wrapper.u_sram.din0 memory_wrapper.encoder_inst.data_i}
 
-# Save Setup Database
-if {$use_setup_database} {
-    save_database -force $setup_database_name
-}
 
-}
+
+
 
 read_sva -version sv2012 [subst {
     $property_path/64B/memory_wrapper_fv_32_2b_cut.sv
 }]
 
 
-####################
-# Check properties #
-if {[string equal [info hostname] elara]} {
-    add_server -max_processes 1 mab.lubis-eda.com
-}
-if {[string equal [info hostname] elara]} {
-    add_server -max_processes 1 atlas.lubis-eda.com
-}
 
-# Configure default task
-# Server configuration
-if {[string equal [info hostname] elara]} {
-    set_check_option -local_processes 1
-    set_check_option -network_processes 2
-    set_check_option -parallel network
-}
-if {[string equal [info hostname] mab.lubis-eda.com]} {
-    set_check_option -local_processes 1
-}
-if {[string equal [info hostname] atlas.lubis-eda.com]} {
-    set_check_option -local_processes 1
-}
-
-if {$auto_check && $auto_witness} {
-    check -force -all [get_assertions] -orchestrator questa
-} elseif {$auto_check} {
-    check -force [get_assertions] -orchestrator questa
-} elseif {$auto_witness} {
-    check -force -pass [get_assertions] -orchestrator questa
-}
-
-if {$exit_after_execution} {
-    exit -force
-}
